80010762a3544fa995577092e6c1e3f25c9e265e
zhuqiyang <250994559@qq.com>
2020-03-14 23:22:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+创建kubernetes证书
+
+把 openssl.cnf 的alt_name改成自己需要的
---
+++ k8s-certs.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+
+mkdir cert/
+cd cert/
+
+cat > openssl.cnf <<EOF
+[req]
+req_extensions = v3_req
+distinguished_name = req_distinguished_name
+[ req_distinguished_name ]
+[ v3_req ]
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = kubernetes
+DNS.2 = kubernetes.default
+DNS.3 = kubernetes.default.svc
+DNS.4 = kubernetes.default.svc.cluster.local
+DNS.5 = k8s-master01
+DNS.6 = k8s-master02
+DNS.7 = k8s-master03
+DNS.8 = k8s-master
+DNS.9 = localhost
+DNS.10 = etcd01
+DNS.11 = etcd02
+DNS.12 = etcd03
+IP.1 = 10.96.0.1
+EOF
+
+
+#
+# example:
+# $1 "ca"
+# $2 "/CN=k8s-ca"
+#
+function openssl_ca() {
+    openssl genrsa -out $1.key 2048
+    openssl req -x509 -new -nodes -key $1.key -subj "$2" -days 5000 -out $1.crt
+}
+
+#
+# example:
+# $1 "ca"
+# $2 "apiserver"
+# $3 "/CN=kube-apiserver"
+#
+function openssl_k8s() {
+    openssl genrsa -out $2.key 2048
+    openssl req -new -key $2.key -subj "$3" -out $2.csr -config openssl.cnf
+    openssl x509 -req -in $2.csr -CA $1.crt -CAkey $1.key -CAcreateserial -out $2.crt -days 5000 -extensions v3_req -extfile openssl.cnf
+}
+
+
+openssl_ca "ca" "/CN=k8s-ca"
+openssl_k8s "ca" "apiserver" "/CN=kube-apiserver"
+openssl_k8s "ca" "apiserver-kubelet-client" "/CN=kube-apiserver-kubelet-client/O=system:masters"
+openssl_k8s "ca" "kube-controller-manager" "/CN=system:kube-controller-manager"
+openssl_k8s "ca" "kube-scheduler" "/CN=system:kube-scheduler"
+openssl_k8s "ca" "kube-proxy" "/CN=system:kube-proxy"
+
+openssl_ca "front-proxy-ca" "/CN=front-proxy-ca"
+openssl_k8s "front-proxy-ca" "front-proxy-client" "/CN=front-proxy-client"
+
+
+openssl genrsa -out sa.key 2048
+openssl rsa -in sa.key -pubout -out sa.pub
+
+
+BOOTSTRAP_TOKEN="$(head -c 6 /dev/urandom | md5sum | head -c 6).$(head -c 16 /dev/urandom | md5sum | head -c 16)"
+echo "$BOOTSTRAP_TOKEN,system:bootstrapper,10001,\"system:bootstrappers\"" > token.csv
+
+
+
+
+
+
+
+
+
+
+
