d7cda66871b7ad091ce68e36433b30a6fd1db776
zhuqiyang <qiqitao320@qq.com>
2020-11-04 09:43:39 -0500

    change file

--- install-k8s-master/01.install-k8s-master.sh
+++ install-k8s-master/01.install-k8s-master.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ###########################################################
 #                                                         #
-#    install api-server、controller-manager、scheduler    #
+#     install api-server controller-manager scheduler     #
 #                                                         #
 ###########################################################
 
